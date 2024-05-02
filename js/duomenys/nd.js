2af4896bb79b7e6ce3d1a876060e68c5155bd80e
Raminta <1989sliekiukasss@gmail.com>
2024-05-02 22:14:12 +0300

    nieko

--- js/duomenys/nd.js
+++ js/duomenys/nd.js
@@ -1,4 +1,36 @@
-console.log(true && true && true && true);
-console.log(false && false && false && false);
-console.log(true && true && true && false);
-console.log(true && true && false && false);
+console.log(true && true && true && true); 
+console.log(true && true && true && false); // true + false = false
+console.log(true && true && false && false); // true + false = false
+console.log(true && false && false && false); // false + false = false
+console.clear();
+console.log(false && false && false && false); 
+console.log(false && false && false && true);  // false + false = false
+console.log(false && false && true && false); // false + false = false
+console.log(false && true && false && false); // false + false = false
+console.log(true && false && false && false); // false + false = false
+console.clear();
+console.log(true || true || true || true);
+console.log(false || false || false || false); 
+console.log(true || true || true || false); // true or false = true
+console.log(true || true || false || false); // true or false = true
+console.log(true || false || false || false); // true or false = true
+console.clear();
+console.log(false || false || false || false); 
+console.log(false || false || false || true);  
+console.log(false || false || true || false); 
+console.log(false || true || false || false); 
+
+console.log(true && true && true || true); 
+console.log(true && true && true || false); 
+console.log(true && true && false || false); 
+console.log(true && false && false || false); 
+
+console.log(true || true && true && true); 
+console.log(true || true && true && false); 
+console.log(true || true && false && false); 
+console.log(true || false && false && false);
+console.clear();
+console.log(true && true || true || true); 
+console.log(true && true || true || false); 
+console.log(true && true || false || false); 
+console.log(true && false || false || false);
