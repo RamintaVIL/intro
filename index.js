829c3230c5a2f621ba196b274b4af66cc4853c6e
Raminta <1989sliekiukasss@gmail.com>
2024-05-02 13:28:57 +0300

    update

---
+++ index.js
@@ -0,0 +1,40 @@
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+// CRTL + S = issaugoti failai
+// CRTL + A = pazymeti visa teksta
+// CRTL + X = iskirpti testa
+// CTRL + Z = gristi vienu langu atgal
+
+
+console.log(19890912);
+console.log(12);
+console.log(3+5);
+console.log(5*5);
+console.log(24/3);
+
+/*
+Pagrindines programavimo temos:
+- duomenu tipai
+- palyginimo salygos (if)
+- ciklai (for)
+- funkcijos
+- class (oop)
+- event listeners
+*/
+
+/*
+Duomenu tipai:
+- number (skaiciai)
+- string (tekstas)
+- boolean (logine reiksme) 
+- array (masyvas)
+- object (objektas, key-value)
+- null
+- undefined
+*/
