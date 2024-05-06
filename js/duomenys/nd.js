d4c3ef517ba271cf8a76c5e5e835624992a891f1
Raminta <1989sliekiukasss@gmail.com>
2024-05-06 21:03:24 +0300

    pradeti nd

--- js/duomenys/nd.js
+++ js/duomenys/nd.js
@@ -46,7 +46,7 @@ console.log(true && true && false || false);
 console.log(true && false && false || false); 
 console.log(true && false && false || true);
 console.log(true && false && tue || false);
-// Ziurint is desines i kaire gausnasi atsakymas
+// Ziurint is desines i kaire gaunasi atsakymas
 console.clear();
 console.log(true && true || true && true); 
 console.log(true && true || true && false); 
@@ -103,7 +103,7 @@ console.log(true || false && false || true);
 console.log(true || false && true || false);
 // console.log(true || (true && false) || false);
 // console.log(true || false || false);
-// cia kaip su klaustais kitu atveju nesigauna atsakymas
+// cia kaip su skliaustais kitu atveju nesigauna atsakymas
 console.clear();
 console.log(false || false || false && false); 
 console.log(false || false || false && true);  
@@ -174,81 +174,140 @@ console.log(false && true || false && true);
 
 console.clear();
 
-// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
-// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
-// 1. Store Mark's and John's mass and height in variables
-// 2. Calculate both their BMIs using the formula (you can even implement both versions)
-// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
-// Test data:
-// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall. 
+// Kintamųjų inicijavimas
+// 1.	Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// a.	Po kiekvieno jų inicijavimo, išvesti į console
+const a = 2;
+console.log(a);
+const b = 8;
+console.log(b);
+const c = 12;
+console.log(c);
 
-let name = 'Mark';
+// 2.	Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+// a.	Po kiekvieno jų inicijavimo, išvesti į console
+const name = 'Petras';
 console.log(name);
-let svoris = 78;
-console.log(svoris);
-let ugis = 1.69;
-console.log(ugis);
+const surname = 'Petraitis'
+console.log(surname);
+const city = 'Kaunas'
+console.log(city);
 
-let information = name + ' ' + svoris + 'kg' + ' ' + ugis + 'm' + '.'; 
-console.log(information);
-
-let name2 = 'John';
-console.log(name2);
-let svoris2 = 92
-console.log(svoris2);
-let ugis2 = 1.95;
-console.log(ugis2);
-
-let information2 = name2 + ' ' + svoris2 + 'kg' + ' ' + ugis2 + 'm' + '.'; 
-console.log(information2);
-
-let nameBMI = svoris / ugis ** 2;
-let name2BMI = svoris2 / ugis2 ** 2;
-
-console.log(nameBMI);
-console.log(name2BMI);
-
-console.clear();
+// 3.	Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+// a.	Po kiekvieno jų inicijavimo, išvesti į console
+const number = [12, 8, 5];
+console.log(number);
 
 
+// 4.	Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+// a.	Po kiekvieno jų inicijavimo, išvesti į console
+const words = ['labas', 'pasauli', 'platus'];
+console.log(words);
 
+// Veiksmai su kintamaisiais
 
-const MarksMass = 95 // kg;
-const MarksTall = 1.88 // m;
+// 1.	Susumuoti visus skaičiaus tipo kintamuosius
+// a.	Rezultatą išvesti į console
+const sum = a + b + c;
+console.log(sum);
 
-const MarksBMI =  MarksMass / MarksTall ** 2;
-console.log(MarksBMI);
+// 2.	Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+// a.	Rezultatą išvesti į console
+const text = name + ' ' + surname + ' ' + city + '.';
+console.log(text);
 
-const JohnWeights = 85 // kg;
-const JohnTall = 1.76 // m;
+// 3.	Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+// a.	1-2+3-4+5
+// b.	Rezultatą išvesti į console
+const marks0 = 12;
+const marks1 = 8;
+const marks2 = 5;
+const number1 = marks0 + marks1 + marks2;
+console.log(number1);
 
-const JohnBMI =  JohnWeights / JohnTall ** 2;
-console.log(JohnBMI);
+// 4.	Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+const w1 = 'platus';
+const w2 = 'pasauli';
+const w3 = 'labas';
+const sentense = [w1 + ' ' + w2 + ',' + w3];
+console.log(sentense);
 
-const compare = MarksTall || JohnTall;
-console.log(compare);
+// Kintamųjų palyginimas
+// Priklausomai nuo sąlygos, susikurkite 2 skirtingų reikšmių nurodyto tipo kintamuosius.
+// Lyginant, jei rezultatas tenkina palyginimo sąlygą (nurodytą žemiau), tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
 
-const isMarkBMIBigger = "John" + " " + JohnBMI || 'Mark' + ' ' + MarksBMI;
-console.log(isMarkBMIBigger);
-
-const isJohnBMIBigger = MarksBMI || JohnBMI;
-console.log(isJohnBMIBigger);
-
-const Mantai = "Mantas";
-
-function vardas(name) {
-    return console.log('Labas ${name}')
+// 1.	Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+// a.	kuris didesnis
+if (8 > 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
 }
-vardas("Mantai");
-
-// Sukurkitr funkciją, kuri priimtu viena parametra (teksta)
-
-function howMany(text) {
-    return console.log(text.lenght)
+// b.	kuris mažesnis
+if (8 < 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+// c.	ar jie lygūs
+if (8 === 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
 }
 
-howMany("Labas")
+// d.	ar jie nelygūs
+if (8 !== 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+// e.	kuris didesnis arba lygus
+if (8 <= 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+// f.	kuris mažesnis arba lygus
+if (8 >= 10) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+
+// 2.	Išvesti teksto tipo kintamųjų ilgius
+const marks = ['Nežinau, kokios spalvos batelius turiu namuose'];
+const marks6 = ['Manau pirksiu geltonos'];
+
+const suma = marks[0] + marks[1] + marks[2] + marks[3] + marks[4] + marks[5];
+let length = marks.length ;
+const ilgis2 = marks6.length ;
+console.log(length);
+console.log(ilgis2);
+
+// 3.	Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+// a.	kuris didesnis
+// b.	kuris mažesnis
+// c.	ar jie lygūs
+// d.	ar jie nelygūs
+// e.	kuris didesnis arba lygus
+// f.	kuris mažesnis arba lygus
+
+// 4.	Išvesti sąrašo tipo kintamųjų ilgius
+
+// 5.	Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+// a.	kuris didesnis
+// b.	kuris mažesnis
+// c.	ar jie lygūs
+// d.	ar jie nelygūs
+// e.	kuris didesnis arba lygus
+// f.	kuris mažesnis arba lygus
+
+
+
+
+
+
 
 
 
