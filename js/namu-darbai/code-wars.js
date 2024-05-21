d394eecea28deb40a7c46a5dd70d10e48d317c49
Raminta <1989sliekiukasss@gmail.com>
2024-05-21 19:24:31 +0300

    try new task

--- js/namu-darbai/code-wars.js
+++ js/namu-darbai/code-wars.js
@@ -255,21 +255,6 @@ console.log(isPythagoreanTriple([3, 5, 9]));
 // //   console.log(findMultiples(4, 27), [4, 8, 12, 16, 20, 24])
 // //   console.log(findMultiples(11, 54), [11, 22, 33, 44])
 
-function arrayMadness(a, b) {
-    // Ready, get set, GO!!!
-  }
-
-  console.log(arrayMadness([4,5,6],[1,2,3]), true);
-  console.log(arrayMadness([5,6,7],[4,5,6]), false);
-  console.log(arrayMadness([4,5,6],[3,4,5]), false);
-  console.log(arrayMadness([3,4,5],[2,3,4]), false);
-  console.log(arrayMadness([2,3,4],[1,2,3]), false);
-  console.log(arrayMadness([1,2,3],[0,1,2]), true);
-  console.log(arrayMadness([5,3,2,4,1],[15]), false);
-  console.log(arrayMadness([2,5,3,4,1],[3,3,3,3,3]), false);
-  console.log(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]), false);
-  console.log(arrayMadness([1,2,3,4,5],[2,2,2,2,2,2,1,1,1]), true);
-  console.log(arrayMadness([2,4,6,8,10,12,14],[1,3,5,7,9,11,13]), false);
 
   function cookie(x){
     if (typeof x === 'string') {
@@ -285,3 +270,25 @@ console.log(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
 console.log(cookie(26), "Who ate the last cookie? It was Monica!")
 console.log(cookie(2.3), "Who ate the last cookie? It was Monica!")
 console.log(cookie(true), "Who ate the last cookie? It was the dog!")
+
+
+console.log('----------------------------------------');
+// https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/train/javascript
+function arrayMadness(a, b) {
+    let sum = 0;
+    let sum1 = 0;
+   for (let i = 0; i < a.length; i++) {
+   sum += a[i] ** 2;
+   } 
+   for (let g = 0; g < b.length; g++) {
+    sum1 += b[g] ** 3;
+   } 
+   if (sum > sum1) {
+    return true;
+   } else {
+    return false;
+   }
+  }
+
+  console.log(arrayMadness([4,5,6],[1,2,3]));
+  console.log(arrayMadness([5,6,7],[4,5,6]));
