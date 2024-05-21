414af6ce1fb06a4d6cf3309ba97f2a704fb40f51
Raminta <1989sliekiukasss@gmail.com>
2024-05-21 19:23:15 +0300

    object

---
+++ js/duomenys/object.js
@@ -0,0 +1,46 @@
+/*
+OBJECT - objektas
+*/
+
+// const empty = {};
+// console.log(empty);
+
+// const studentName = 'Jonas' ;
+// const studentAge = 99;
+// const studentIsMarried = true;
+
+// const student1Name = 'Maryte' ;
+// const student1Age = 88;
+// const student1IsMarried = false;
+
+// const student2Name = 'Petras' ;
+// const student2Age = 77;
+// const student2IsMarried = false;
+
+// const student3Name = 'Ona' ;
+// const student3Age = 66;
+// const student3IsMarried = true;
+
+// const stud = ['Jonas', 99, true];
+// const stud1 = ['Maryte', 88, false];
+// const stud2 = ['Petras', 77, false];
+// const stud3 = ['Ona', 66, true];
+
+// console.log(stud2[1]);
+
+const stud1 = {
+    name: 'Jonas',
+    age: 99,
+    isMarried: true,
+};
+console.log(stud1);
+console.log(stud1.name);
+console.log(stud1.age);
+console.log(stud1.isMarried);
+
+const stud2 = {
+    age: 88,
+    isMarried: false,
+    name: 'Maryte',
+};
+console.log(stud2);
