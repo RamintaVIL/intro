2e811040795a715a401b116c8f84de2b883f635e
Raminta <1989sliekiukasss@gmail.com>
2024-05-23 11:51:13 +0300

    new exercise

--- js/namu-darbai/codeWars.js
+++ js/namu-darbai/codeWars.js
@@ -1,39 +1,38 @@
 // https://www.codewars.com/kata/571effabb625ed9b0600107a
 
-// function getLength(arr) {
-//     return arr.length;
-// }
-// function getFirst(arr) {
-//     let f = arr[0];
-//     return (f);
-// }
-// function getLast(arr) {
-//     let l = arr[arr.length - 1];
-//     return (l);
-// }
-// function pushElement(arr) {
-//     let el = 1;
-//     arr.push(el);
-//     return arr;
-// }
-// function popElement(arr) {
-//     arr.pop();
-//     return arr;
-// }
+function getLength(arr) {
+    return arr.length;
+}
+function getFirst(arr) {
+    let f = arr[0];
+    return (f);
+}
+function getLast(arr) {
+    let l = arr[arr.length - 1];
+    return (l);
+}
+function pushElement(arr) {
+    let el = 1;
+    arr.push(el);
+    return arr;
+}
+function popElement(arr) {
+    arr.pop();
+    return arr;
+}
 
-//  console.log(getLength([1,2,3]));
-//  console.log(getFirst([1,2,3]));
-//  console.log(getLast([1,2,3]));
-//  console.log(pushElement([1,2,3]));
-//  console.log(popElement([1,2,3]));
+ console.log(getLength([1,2,3]));
+ console.log(getFirst([1,2,3]));
+ console.log(getLast([1,2,3]));
+ console.log(pushElement([1,2,3]));
+ console.log(popElement([1,2,3]));
 
-// // arrow:
+// // arrow funkcija parašyta:
 //  const getLength = arr => arr.length;
 //  const getFirst = arr => arr[0];
 //  const getLast = arr => arr[arr.length - 1];
 //  const pushElement = arr => arr.push(4);
 //  const popElement = arr => arr.pop();
-
 
 // console.log(getLength([1,2,3]));
 // console.log(getFirst([1,2,3]));
@@ -41,10 +40,6 @@
 // console.log(pushElement([1,2,3]));
 // console.log(popElement([1,2,3]));
     
-// const pushElement = arr => [...arr, 1];
-// console.log(pushElement([1,2,3]).length,4);
-
-
 // https://www.codewars.com/kata/5865918c6b569962950002a1/train/javascript
 function strCount(str, letter){ 
     let count = 0; 
@@ -63,3 +58,13 @@ function strCount(str, letter){
   console.log(strCount('Hello', 'o'));
   console.log(strCount('Hello', 'l'));
   console.log(strCount('', 'z'));
+
+ // https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
+//  "This white dog has 4 legs."
+ function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+  }
+  console.log(animal({name:"dog",legs:4,color:"white"}));
+  console.log(animal({name:"cock",legs:2,color:"red"}));
+  console.log(animal({name:"rabbit",legs:4,color:"gray"}));
+
