a96a3b5922cce0379368583b7200fa96c8022e82
Raminta <1989sliekiukasss@gmail.com>
2024-05-23 11:51:34 +0300

    write comments

--- js/duomenys/object.js
+++ js/duomenys/object.js
@@ -87,7 +87,7 @@ function intro(studentdata) {
     }    
 }
 
-// kad butu lengviau skaityti
+// kad butu lengviau skaityti kodą
 function intro(studentData) { 
     const status = studentData.isMarried ? 'esu' : 'nesu';
     return `Sveiki, as ${studentData.name}, man ${studentData.age} ir ${status} vedes ir mano pazymiai ${studentData.marks} yra.`;   
@@ -97,9 +97,77 @@ function intro(studentData) {
         console.log(intro(students[i]));
     }
 
+// 05.22 paskaita
+const stud3 = {
+    name: 'Petras',
+    age: 77,
+    isMarried: false,
+}
+// Supaprastinta objekto sintakse. Panaudojame mažiau simbolių, nes dedame .ir iš karto kreipiamės. 
+console.log(stud3.name); // pavadinimas 'name' turi būti konkretūs egzistuojantys objekte
+console.log(stud3.age);
+console.log(stud3.isMarried);
 
+// Standartine objekto sintakse
+console.log(stud3['name']);
+console.log(stud3['age']);
+console.log(stud3['isMarried']);
 
+const key = 'age';       // čia mano rakto pavadinimas
+console.log(stud3.key); // panasu kad kode key nera, o mums reikia irasyti kintamojo pavadinima 
+console.log(stud3['age']);
+console.log(stud3[key]);
 
+//              data - grąžina orginalius duomenis, data.key - grąžina undefined, nes objekte ieško, duomens key. 
+function giveMe(data, key) {
+    return data[key];
+}
 
+console.log(giveMe({name: 'Jonas', age: 99}, 'name'));
+console.log(giveMe({name: 'Jonas', age: 99}, 'age'));
+console.log(giveMe({name: 'Jonas', age: 99}, 'isMarried'));
+console.log(giveMe({title: 'Svente', page: 1, size: 3}, 'title'));
+console.log(giveMe({title: 'Svente', page: 2, size: 3}, 'page'));
+console.log(giveMe({title: 'Svente', page: 2, size: 3}, 'size'));
+console.log(giveMe({title: 'Svente', page: 2, size: 3}, 'xxx'));  // šito negali grąžinti, nes šitas key neegzistuoja
 
+function basicOp(n1, operation, n2){
+ const mathFunctions = {
+      '+': (a, b) => a + b,  // kiekvienas raktas reprezentuoja function, bet spausdinant reikia nepamiršti(reikia nurodyti kintamuosius)
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+      '**': (a, b) => a ** b,
+      '0': () => 0,
+ }
+ return mathFunctions[operation](n1, n2);
+}
 
+console.log(basicOp(5, '+', 7));
+console.log(basicOp(5, '-', 7));
+console.log(basicOp(5, '*', 7));
+console.log(basicOp(5, '/', 7));
+console.log(basicOp(5, '**', 7));
+console.log(basicOp(5, '0', 7));
+
+// kitas pvz galėjo būti:
+const sum = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multi = (a, b) => a * b;
+const div= (a, b) => a + b;
+
+function basicOp(n1, operation, n2){
+    const mathFunctions = {
+         '+': sum,  
+         '-': minus,
+         '*': multi,
+         '/': div,
+    }
+    return mathFunctions[operation](n1, n2);
+   }
+   
+   console.log(basicOp(5, '+', 7));
+   console.log(basicOp(5, '-', 7));
+   console.log(basicOp(5, '*', 7));
+   console.log(basicOp(5, '/', 7));
+   
