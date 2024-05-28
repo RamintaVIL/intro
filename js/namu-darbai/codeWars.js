d034ac2c146a1a125dc229580f5ae94cc4d04534
Raminta <1989sliekiukasss@gmail.com>
2024-05-28 11:56:29 +0300

    good or bad

--- js/namu-darbai/codeWars.js
+++ js/namu-darbai/codeWars.js
@@ -187,6 +187,27 @@ console.log(bmi(110, 1.70));
 console.log(bmi(75, 1.70));
 console.log(bmi(45, 1.70));
 
+// https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript
+
+function well(x){
+    let goodCount = 0;
+    for (let i = 0; i < x.length; i++) {
+       if (x[i] === 'good') {
+        goodCount++;
+       }
+    }   
+    if (goodCount === 0) {
+        return 'Fail!';
+    } else if (goodCount <= 2) {
+        return 'Publish!';
+    } else {
+        return 'I smell a series!'
+    }
+}     
+
+console.log(well(['bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']));
 
 
 
