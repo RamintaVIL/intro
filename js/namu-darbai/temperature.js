bc63c42453d2aa8da22c1f9d3d6d3e76c90b119e
Raminta <1989sliekiukasss@gmail.com>
2024-05-30 12:00:48 +0300

    temperature

---
+++ js/namu-darbai/temperature.js
@@ -0,0 +1,18 @@
+function weatherInfo (temp) {
+    let c = convertToCelsius(temp);
+    if (c <= 0) {
+      return c + " is freezing temperature";
+     } else {
+      return c + " is above freezing temperature";
+       }
+  }
+  
+  function convertToCelsius (temperature) {
+    let celsius = (temperature - 32) * (5/9);
+    return celsius;
+  }
+
+console.log(weatherInfo(50));
+console.log(weatherInfo(23));
+
+
